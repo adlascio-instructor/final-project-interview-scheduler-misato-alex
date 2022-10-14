9f2fef4bc54b1f95a3247efb7d3e3ea2255186ec
alejandroguerravillegas <coreartalex2@gmail.com>
2022-10-14 09:57:19 -0700

    create & delete queries for interviews and available

--- backend/database/queries/test_queries.sql
+++ backend/database/queries/test_queries.sql
@@ -1,5 +1,17 @@
 -- SELECT * FROM appointments;
 
-SELECT * FROM available;
+-- SELECT * FROM available;
 
 -- SELECT * FROM interviewers;
+
+-- create interview query
+INSERT INTO interviews (interviewer_id, appointment_id, student_name) VALUES ($1,$2,$3);
+
+-- delete interview query
+DELETE FROM interviews WHERE interview_id = $1;
+
+-- create available query
+INSERT INTO interviews (interviewer_id, day_id) VALUES ($1,$2);
+
+-- delete available entry query
+DELETE FROM available WHERE available_id = $1;
